930fefe7397653bbe4be04fbfb966920ddf63b13
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2025-09-26 15:13:15 +0300

    feat: add init of AdjacencyMatrixFA

--- project/regular_query.py
+++ project/regular_query.py
@@ -1,7 +1,7 @@
 from typing import Iterable
 import numpy as np
 from pyformlang.finite_automaton import NondeterministicFiniteAutomaton
-from pyformlang.finite_automaton import Symbol
+from pyformlang.finite_automaton import State
 from dataclasses import dataclass
 from scipy.sparse import coo_array
 
@@ -14,29 +14,19 @@ class AdjacencyMatrixFA:
     def __init__(self, nfa: NondeterministicFiniteAutomaton):
         self.start_states = nfa.start_states
         self.final_states = nfa.final_states
-
-        # dict(node, dict[str, node]) ==
-        # dict(int, dict[str, int])
         delta = nfa.to_dict()
-
-        row = dict[list]
-        col = dict[list]
-
-        # iterate over delta.keys() (v)
-        # for every v.key (s) with value v.value (u)
-        # add pair of (v, u, 1) into coo_array
-        # and add it to matrix[s]
-        for v, edge in delta.items():
-            for s, u in edge.items():
-                row[s].append(v)
-                col[s].append(u)
-
-        for s in delta.symbols:
+        print(delta)
+        row = {k: [] for k in nfa.symbols}
+        col = {k: [] for k in nfa.symbols}
+        for v1, edge in delta.items():
+            for s, v2 in edge.items():
+                row[s].append(v1.value)
+                for v in v2:
+                    col[s].append(v.value)
+                print(f"added {v1, s, v2}")
+        self.matrix = {}
+        for s in nfa.symbols:
             data = [1] * len(row[s])
-            coo = coo_array(data, (row[s], col[s]))
+            n = len(nfa.states)
+            coo = coo_array((data, (row[s], col[s])), shape = (n, n))
             self.matrix[s] = coo
-
-
-
-
-
