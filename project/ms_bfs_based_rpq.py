ff16833216e458bb567020744a7f3e044d800cdf
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2025-12-28 09:25:07 +0300

    fix: ms bfs algorithm logic

--- project/ms_bfs_based_rpq.py
+++ project/ms_bfs_based_rpq.py
@@ -1,72 +1,67 @@
 from networkx import MultiDiGraph
 from project.finite_automata import regex_to_dfa, graph_to_nfa
 from project.regular_query import AdjacencyMatrixFA
-from collections import deque
-
-
-def create_trans_dict(automaton: AdjacencyMatrixFA) -> dict[str, dict[int, set[int]]]:
-    transitions = {}
-    for symbol, matrix in automaton.adj_matrices.items():
-        transitions[symbol] = {}
-        coo = matrix.tocoo()
-        for i, j in zip(coo.row, coo.col):
-            if i not in transitions[symbol]:
-                transitions[symbol][i] = set()
-            transitions[symbol][i].add(j)
-    return transitions
+from scipy import sparse
 
 
 def ms_bfs_based_rpq(
     regex: str, graph: MultiDiGraph, start_nodes: set[int], final_nodes: set[int]
 ) -> set[tuple[int, int]]:
-    regex_dfa = regex_to_dfa(regex)
-    graph_nfa = graph_to_nfa(graph, start_nodes, final_nodes)
-
-    nfaG = AdjacencyMatrixFA(graph_nfa)
-    dfaQ = AdjacencyMatrixFA(regex_dfa)
-
-    nfa_transitions = create_trans_dict(nfaG)
-    dfa_transitions = create_trans_dict(dfaQ)
-
-    abc = set(nfa_transitions.keys()) & set(dfa_transitions.keys())
-
-    nfa_index_to_state = {v: k for k, v in nfaG.state_to_index.items()}
-    dfa_index_to_state = {v: k for k, v in dfaQ.state_to_index.items()}
-
-    pairs = set()
-
-    for start_node in start_nodes:
-        if start_node not in nfaG.state_to_index:
-            continue
-
-        start_nfa_idx = nfaG.state_to_index[start_node]
-        visited = set()
-        queue = deque()
-
-        for dfa_start in dfaQ.start_states:
-            start_dfa_idx = dfaQ.state_to_index[dfa_start]
-            start_state = (start_nfa_idx, start_dfa_idx)
-            visited.add(start_state)
-            queue.append(start_state)
-
-        while queue:
-            nfa_idx, dfa_idx = queue.popleft()
-
-            nfa_state = nfa_index_to_state[nfa_idx]
-            dfa_state = dfa_index_to_state[dfa_idx]
-
-            if nfa_state in final_nodes and dfa_state in dfaQ.final_states:
-                pairs.add((start_node, nfa_state))
-
-            for symbol in abc:
-                nfa_nexts = nfa_transitions[symbol].get(nfa_idx, set())
-                dfa_nexts = dfa_transitions[symbol].get(dfa_idx, set())
-
-                for nfa_next_idx in nfa_nexts:
-                    for dfa_next_idx in dfa_nexts:
-                        next_state = (nfa_next_idx, dfa_next_idx)
-                        if next_state not in visited:
-                            visited.add(next_state)
-                            queue.append(next_state)
-
-    return pairs
+    regex_adj = AdjacencyMatrixFA(regex_to_dfa(regex))
+    if start_nodes is None:
+        start_nodes = set()
+    if final_nodes is None:
+        final_nodes = set()
+    graph_adj = AdjacencyMatrixFA(graph_to_nfa(graph, start_nodes, final_nodes))
+    front_blocks = []
+    graph_state_to_idx = graph_adj.state_to_index
+    regex_state_to_idx = regex_adj.state_to_index
+    common_symbols = set(graph_adj.adj_matrices.keys()) & set(
+        regex_adj.adj_matrices.keys()
+    )
+    for graph_start_state in graph_adj.start_states:
+        graph_start_state_idx = graph_state_to_idx[graph_start_state]
+        front_block = sparse.csr_array(
+            (graph_adj.states_num, regex_adj.states_num), dtype=bool
+        )
+        for regex_start_state in regex_adj.start_states:
+            regex_start_state_idx = regex_state_to_idx[regex_start_state]
+            front_block[graph_start_state_idx, regex_start_state_idx] = True
+        front_blocks.append(front_block)
+    front = sparse.vstack(front_blocks)
+    visited = front.copy()
+    graph_adj_matrices = graph_adj.adj_matrices
+    regex_adj_matrices = regex_adj.adj_matrices
+    graph_adj_matrices_t = {
+        symbol: decomposition.transpose().tocsr()
+        for symbol, decomposition in graph_adj_matrices.items()
+    }
+    while front.nnz:
+        new_front_blocks = {}
+        for symbol in common_symbols:
+            this_symbol_blocks = []
+            for i in range(len(graph_adj.start_states)):
+                current_block = front[
+                    i * graph_adj.states_num : (i + 1) * graph_adj.states_num
+                ]
+                step = (
+                    graph_adj_matrices_t[symbol]
+                    @ current_block
+                    @ regex_adj_matrices[symbol]
+                )
+                this_symbol_blocks.append(step)
+            new_front_blocks[symbol] = sparse.vstack(this_symbol_blocks)
+        front = sum(new_front_blocks.values()) > visited
+        visited = visited + front
+    result = set()
+    for i, graph_start_state in enumerate(graph_adj.start_states):
+        result_block = visited[
+            i * graph_adj.states_num : (i + 1) * graph_adj.states_num
+        ]
+        for graph_final_state in graph_adj.final_states:
+            graph_final_idx = graph_state_to_idx[graph_final_state]
+            for regex_final_state in regex_adj.final_states:
+                regex_final_idx = regex_state_to_idx[regex_final_state]
+                if result_block[graph_final_idx, regex_final_idx]:
+                    result.add((graph_start_state.value, graph_final_state.value))
+    return result
