76e65316b11057a4023577c3b152b540d4d62aaf
Vlasenco Daniel <vlasenko.daniil26@gmail.com>
2026-01-04 16:59:28 +0300

    ref: reformat code

--- project/matrix_based_cfpq.py
+++ project/matrix_based_cfpq.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import pyformlang.cfg as cfg
 import scipy.sparse as sp
-from typing import Set, Tuple, Dict, Any
+from typing import Set
 from project.hellings import cfg_to_weak_normal_form
 
 
@@ -12,55 +12,32 @@ def matrix_based_cfpq(
     final_nodes: Set[int] = None,
 ) -> set[tuple[int, int]]:
     transformed_cfg = cfg_to_weak_normal_form(cfg)
-    production_map = build_mappings(transformed_cfg)
-
-    matrices = initialize_matrices(graph, transformed_cfg)
-    update_matrices(matrices, production_map)
-
-    index_to_node = {idx: node for idx, node in enumerate(graph.nodes)}
-    return extract_result(
-        matrices, transformed_cfg.start_symbol, start_nodes, final_nodes, index_to_node
-    )
-
-
-def build_mappings(cfg: cfg.CFG) -> Tuple[Dict[Any, Set], Dict[Tuple, Set]]:
     production_map = {}
-
-    for prod in cfg.productions:
+    for prod in transformed_cfg.productions:
         if len(prod.body) == 2:
             production_map.setdefault(tuple(prod.body), set()).add(prod.head)
 
-    return production_map
-
-
-def initialize_matrices(graph: nx.DiGraph, cfg: cfg.CFG) -> Dict[Any, sp.csc_matrix]:
     node_count = graph.number_of_nodes()
     node_index = {node: idx for idx, node in enumerate(graph.nodes)}
-
+    index_to_node = {idx: node for idx, node in enumerate(graph.nodes)}
     matrices = {
         var: sp.csc_matrix((node_count, node_count), dtype=bool)
-        for var in cfg.variables
+        for var in transformed_cfg.variables
     }
 
     for start, end, label in graph.edges(data="label"):
-        for variable in cfg.variables:
+        for variable in transformed_cfg.variables:
             if any(
                 prod.body[0].value == label
-                for prod in cfg.productions
+                for prod in transformed_cfg.productions
                 if prod.head == variable and len(prod.body) == 1
             ):
                 matrices[variable][node_index[start], node_index[end]] = True
 
     for node in graph.nodes:
-        for variable in cfg.get_nullable_symbols():
+        for variable in transformed_cfg.get_nullable_symbols():
             matrices[variable][node_index[node], node_index[node]] = True
 
-    return matrices
-
-
-def update_matrices(
-    matrices: Dict[Any, sp.csc_matrix], production_map: Dict[Tuple, Set]
-) -> None:
     update_queue = list(matrices.keys())
 
     while update_queue:
@@ -77,20 +54,20 @@ def update_matrices(
                     if head not in update_queue:
                         update_queue.append(head)
 
-
-def extract_result(
-    matrices: Dict[Any, sp.csc_matrix],
-    start_symbol: Any,
-    start_nodes: Set[int],
-    final_nodes: Set[int],
-    index_to_node: Dict[int, Any],
-) -> Set[Tuple[Any, Any]]:
-    if start_symbol not in matrices:
+    if transformed_cfg.start_symbol not in matrices:
         return set()
 
-    result_matrix = matrices[start_symbol]
-    return {
+    result_matrix = matrices[transformed_cfg.start_symbol]
+
+    if start_nodes is None:
+        start_nodes = set(graph.nodes)
+    if final_nodes is None:
+        final_nodes = set(graph.nodes)
+
+    res = {
         (index_to_node[i], index_to_node[j])
         for i, j in zip(*result_matrix.nonzero())
         if index_to_node[i] in start_nodes and index_to_node[j] in final_nodes
     }
+
+    return res
