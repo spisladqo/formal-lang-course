900bb08d9825ebc261ca0023c267fa415b2bb248
Vlasenco Daniel <vlasenko.daniil26@gmail.com>
2026-01-05 22:26:10 +0300

    fix: import in tests

--- tests/autotests/test_task09.py
+++ tests/autotests/test_task09.py
@@ -18,7 +18,7 @@ try:
     from project.hellings import hellings_based_cfpq
     from project.matrix_based_cfpq import matrix_based_cfpq
     from project.tensor_based_cfpq import tensor_based_cfpq, cfg_to_rsm, ebnf_to_rsm
-    from project.task9 import gll_based_cfpq
+    from project.gll_based_cfpq import gll_based_cfpq
 except ImportError:
     pytestmark = pytest.mark.skip("Task 9 is not ready to test!")
 
